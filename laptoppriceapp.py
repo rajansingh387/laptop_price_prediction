4bab4363fa504a9c8bccae9622f2fc32d92027c8
rajansingh387 <119110954+rajansingh387@users.noreply.github.com>
2023-04-27 18:51:05 +0530

    Update laptoppriceapp.py

--- laptoppriceapp.py
+++ laptoppriceapp.py
@@ -26,3 +26,16 @@ ssd = st.selectbox('SSD(GB)',[128, 0,256,512,32,64,1000,1024,16,768,180,240,8])
 gpu = st.selectbox('GPU',df['gpu brand'].unique())
 os = st.selectbox('OS',df['os'].unique())
 
+if st.button('Predict Laptop Price'):
+    if touchscreen=='Yes':
+        touchscreen=1
+    else:
+        touchscreen=0
+    if ips=='Yes':
+        ips=1
+    else:
+        ips=0
+    test = np.array([company,type,ram,weight,touchscreen,ips,cpu,hdd,ssd,gpu,os])
+    test = test.reshape([1,11])
+
+    st.success(model.predict(test)[0])
